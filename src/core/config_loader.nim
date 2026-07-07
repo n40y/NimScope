c413785580adcec0e1eb46934ebd7e5a808359b7
N40y <131626661+n40y@users.noreply.github.com>
2026-07-07 19:53:15 +0200

    Update config_loader.nim

--- src/core/config_loader.nim
+++ src/core/config_loader.nim
@@ -1,29 +1,29 @@
 # src/core/config_loader.nim
 
-import std/[os, json]
+import std/json
+import loader 
 
 type
   AdPorts* = object
     ldap*: int
-    ldaps*: int
     smb*: int
     kerberos*: int
-    winrm*: int
 
-  DefaultQueries* = object
-    naming_contexts*: string
+  LdapQueries* = object
+    base_dn*: string
 
   AdConfig* = object
     ad_ports*: AdPorts
-    default_queries*: DefaultQueries
+    ldap_queries*: LdapQueries
 
 proc loadAdConfig*(path: string = "config/ad_defaults.json"): AdConfig =
-  if not fileExists(path):
-    # Si le fichier de config n'existe pas, on renvoie des valeurs hardcodées de secours
-    return AdConfig(
-      ad_ports: AdPorts(ldap: 389, ldaps: 636, smb: 445, kerberos: 88, winrm: 5985),
-      default_queries: DefaultQueries(naming_contexts: "(objectClass=*)")
+  try:
+    let jsonNode = parseFile(path)
+    result = to(jsonNode, AdConfig)
+  except CatchableError as e:
+    echo "[!] Error loading global configuration file: " & e.msg
+    # Fallback default values
+    result = AdConfig(
+      ad_ports: AdPorts(ldap: 389, smb: 445, kerberos: 88),
+      ldap_queries: LdapQueries(base_dn: "DC=domain,DC=local")
     )
-  
-  let node = parseFile(path)
-  return node.to(AdConfig)
