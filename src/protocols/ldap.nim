3a70670df8686c668489bb21820bd24c9c4689ab
N40y <131626661+n40y@users.noreply.github.com>
2026-07-09 20:23:01 +0200

    Add protocol wrappers for backward compatibility with executor imports

---
+++ src/protocols/ldap.nim
@@ -0,0 +1,4 @@
+# src/protocols/ldap.nim
+# Wrapper qui réexporte le module LDAP d'AD
+
+include ../protocols/ad/ldap
---
+++ src/protocols/smb.nim
@@ -0,0 +1,4 @@
+# src/protocols/smb.nim
+# Wrapper qui réexporte le module SMB d'AD
+
+include ../protocols/ad/smb
